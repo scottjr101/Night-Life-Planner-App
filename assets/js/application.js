a563dc90dc3c1f4315ec84fa5f0e5675d57ea4c6
Foster Gailey <foster@gaileyhome.com>
2019-10-10 13:56:44 -0400

    pulling events from TM

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,13 +10,32 @@ $(document).on('DOMContentLoaded', () => {
   });
 
   $(document).ready(function(){
-    let queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=f7iOI1K6ZSelrJQmQ9kZrXMGns1biEKR&postalCode=30092"
+
+    let apikey = "f7iOI1K6ZSelrJQmQ9kZrXMGns1biEKR";
+    //default postal code
+    let TMpostCode = "30092";
+
+    let TMkeyword = "football";
+
+    let TMevents = "/discovery/v2/events";
+
+    let TMsuggest = "/discovery/v2/suggest";
+
+    let queryURL = `https://app.ticketmaster.com${TMevents}.json?apikey=${apikey}&countryCode=us`
 
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+        dataType: "json",
+
       }).then(function(response) {
         console.log(response);
+        let TM = response._embedded.events
+
+        for (let a = 0; a < TM.length; a++){
+            console.log(TM[a]);
+            
+        }
       });
 
 
