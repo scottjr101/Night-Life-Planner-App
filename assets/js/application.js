6807e6d6d9dd6a2a2c9bb8c1a891b7fa16f159c0
Foster Gailey <foster@gaileyhome.com>
2019-10-10 19:37:57 -0400

    added new button for event

--- assets/js/application.js
+++ assets/js/application.js
@@ -14,47 +14,6 @@ $(document).on('DOMContentLoaded', () => {
 
     $("#add-movie-btn").on("click", (event)=>{
 
-    event.preventDefault();
-
-
-    let TMapikey = "f7iOI1K6ZSelrJQmQ9kZrXMGns1biEKR";
-    //default postal code
-    let TMpostCode = $("#zip-code-input").val().trim();
-
-    let TMradius = $("#radius-input").val().trim()
-    
-    let TMkeyword = "concert";
-    
-    let TMevents = "/discovery/v2/attractions";
-
-    let TMsuggest = "/discovery/v2/suggest";
-
-        console.log(TMradius)
-        console.log(TMpostCode)
-    let TMqueryURL = `https://app.ticketmaster.com${TMsuggest}.json?TM=${TMapikey}&postalCode=${TMpostCode}&radius=${TMradius}&keyword=${TMkeyword}`
-
-    $.ajax({
-        url: TMqueryURL,
-        method: "GET",
-        dataType: "json",
-
-      }).then(function(response) {
-        console.log(response);
-        let TM = response._embedded.attractions
-
-        for (let a = 0; a < TM.length; a++){
-            console.log(TM[a]);
-            
-            let nDiv = $("<div>");
-            $("#local-events").append(nDiv);
-
-            let nImg = $("<img>");
-            nImg.attr("src", TM[a].images[0].url);
-
-            $(nDiv).append(nImg)
-        }
-        
-      });
       event.preventDefault();
 
         var startDate = $("#start-date-input").val().trim();
@@ -84,6 +43,51 @@ $(document).on('DOMContentLoaded', () => {
     })
 
 })
+$("#add-event-btn").on("click", (event)=>{
+
+    event.preventDefault();
+
+
+    let apikey = "f7iOI1K6ZSelrJQmQ9kZrXMGns1biEKR";
+    //default postal code
+    let TMpostCode = $("#tm-zip-code-input").val().trim();
+
+    let TMradius = $("#tm-radius-input").val().trim();
+    
+    let TMkeyword = "concert";
+    
+    let TMevents = "/discovery/v2/attractions";
+
+    let TMsuggest = "/discovery/v2/suggest";
+
+        console.log(TMradius)
+        console.log(TMpostCode)
+    let queryURL = `https://app.ticketmaster.com${TMsuggest}.json?apikey=${apikey}&postalCode=${TMpostCode}&radius=${TMradius}&keyword=${TMkeyword}`
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "json",
+
+      }).then(function(response) {
+        console.log(response);
+        let TM = response._embedded.attractions
+
+        for (let a = 0; a < TM.length; a++){
+            console.log(TM[a]);
+            
+            let nDiv = $("<div>");
+            $("#local-events").append(nDiv);
+
+            let nImg = $("<img>");
+            nImg.attr("src", TM[a].images[0].url);
+
+            $(nDiv).append(nImg)
+        }
+        
+      });
+
+    })
 
 
      
