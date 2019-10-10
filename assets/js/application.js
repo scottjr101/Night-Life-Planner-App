c86032ff8bcecc9d8c8ff8a63a3a3a7861b419c7
Foster Gailey <foster@gaileyhome.com>
2019-10-10 13:24:56 -0400

    ticketmaster ajax working

--- assets/js/application.js
+++ assets/js/application.js
@@ -8,3 +8,17 @@ $(document).on('DOMContentLoaded', () => {
     M.Collapsible.init(items);
   
   });
+
+  $(document).ready(function(){
+    let queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=f7iOI1K6ZSelrJQmQ9kZrXMGns1biEKR&postalCode=30092"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+
+
+
+})
